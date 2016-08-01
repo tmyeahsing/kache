fb125ed77f3079bab3ab035305ca9aa382eaa844
yeahsing <yeahsing13@hotmail.com>
2016-08-02 00:59:28 +0800

    接单报价

--- src/entries/order_detail_admin.js
+++ src/entries/order_detail_admin.js
@@ -1,4 +1,10 @@
 'use strict'
+
+Vue.transition('zoom', {
+    enterClass: 'zoomIn',
+    leaveClass: 'zoomOut'
+});
+
 $.promiseAjax({
     url: 'https://api.leancloud.cn/1.1/classes/Order/' + UrlParams.id,
     beforeSend(xhr, setting){
@@ -17,13 +23,46 @@ $.promiseAjax({
         el: '#main_contain',
         data: {
             order: data,
-            statusMap: OrderStatusMap
+            statusMap: OrderStatusMap,
+            isPriceFormulatorShown: false,
+            dropInFee: undefined,
+            manHours: undefined,
+            manUnitPrice: undefined,
+            partsTotal: undefined,
+            partsDesc: ''
         },
         created(){
             var self = this;
         },
         methods: {
-
+            showPriceFormulator(){
+                this.isPriceFormulatorShown = true;
+            },
+            hidePriceFormulator(){
+                this.isPriceFormulatorShown = false;
+            },
+            priceFormulatorTouch(ev){
+                ev.preventDefault();
+            },
+            takeOrder(){
+                var self = this;
+                $.promiseAjax({
+                    url: '/api/order/take',
+                    type: 'put',
+                    data: {
+                        drop_in_fee: self.dropInFee,
+                        man_hours: self.manHours,
+                        man_unit_price: self.manUnitPrice,
+                        parts_total: self.partsTotal,
+                        parts_desc: self.partsDesc,
+                        order_object_id: UrlParams.id
+                    }
+                }).then(function(data){
+                    console.log(data)
+                }).catch(function(err){
+                    console.log(JSON.parse(err.responseText));
+                })
+            }
         }
     })
 })
